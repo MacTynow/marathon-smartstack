abbd2b74e33490be7b3d5b83302abcebb69a7275
Charles Martinot <charles@pocketplaylab.com>
2015-10-01 18:27:57 +0700

    added conf builder function

--- client.rb
+++ client.rb
@@ -4,27 +4,46 @@ require 'em-http'
 require 'em-eventsource'
 require 'json'
 require 'em-http/middleware/json_response'
-require 'yajl'
+require 'socket'
 
 # TODO : handle first time service runs
 
 def parse(stream)
   stream.each_line do |line|
-    puts line
-    test = JSON.parse line
-    puts test['slaveId']
-    puts test['appId']
-    puts test['host']
-    puts test['ports']
-    puts '====================================================================='
+    yield JSON.parse line
   end
 end
 
-def write_config(service, port)
-  target = open('test.txt', 'w')
-  target.write (service)
-  target.write (port)
-  target.write("\n")
+def build_nerve_json(host, service, port, zk_hosts, checks )
+  conf = {
+    :host => host,
+    :port => port,
+    :reporter_type => "zookeeper",
+    :zk_hosts => zk_hosts,
+    :zk_path => "/services/#{service}",
+    :check_interval => 2,
+    :checks => checks
+  }
+end
+
+# This will actually be done when creating a new service...
+# def build_synapse_json(service, zk_hosts, ha_port)
+#   conf = {
+#     :discovery => {
+#       :method => "zookeeper",
+#       :path => "/services/#{service}",
+#       :hosts => zk_hosts
+#     },
+#     :haproxy => {
+#       :port => ha_port,
+#       :listen => ["mode http"]
+#     }
+#   }
+# end
+
+def write_config(service, conf)
+  target = open('#{service}.json', 'w')
+  target.write (conf)
   target.close
 end
 
@@ -33,10 +52,15 @@ EM.run do
   source.inactivity_timeout = 0
   source.retry = 1 
 
+  zk_hosts = ENV.fetch('ZK_HOSTS')
+
+  # TODO determine hostname
+  #host = socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+
   source.message do |message|
-    test = parse(message)
-    # puts test['eventType']
-    write_config(test['eventType'], test['id'])
+    parsed_message = parse(message)
+    conf = build_nerve_json(host, parsed_message['appId'], parsed_message['port'], zk_hosts, parsed_message['checks'])
+    write_config(parsed_message['appId'], conf)
     # puts "new message #{message}"
   end
 
