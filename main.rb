b1c4ed4b9890149854cf78211901fc656f414b6a
Charles Martinot <charles@pocketplaylab.com>
2015-10-22 18:23:25 +0700

    added writing synapse conf

--- main.rb
+++ main.rb
@@ -4,9 +4,6 @@ require 'json'
 require 'net/http'
 require './client.rb'
 
-# These should be passed by salt
-ichnaea = Ichnaea.new
-
 zk_hosts = ENV.fetch('ZK_HOSTS')
 host = ENV.fetch('HOSTNAME')
 ip = ENV.fetch('IP')
@@ -14,12 +11,12 @@ nerve_config_path = ENV.fetch('NERVE') || '/etc/nerve/services'
 synapse_config_path = ENV.fetch('SYNAPSE') || '/etc/synapse/services'
 marathon = ENV.fetch('MARATHON')
 
+
+ichnaea = Ichnaea.new
+
 apps = ichnaea.request("http://#{marathon}:8080/v2/apps")
 
 apps['apps'].each do |app|
-  synapse_conf = ichnaea.build_synapse_json(zk_hosts, app)
-  ichnaea.write_config(synapse_config_path, app['id'], synapse_conf)
-
   target = ichnaea.request("http://#{marathon}:8080/v2/apps/#{app['id']}")
   if target['app']['healthChecks'].empty? == false
     wrote_file = false
@@ -28,6 +25,11 @@ apps['apps'].each do |app|
     if target['app']['container']['docker'].key?('portMappings')
       target['app']['tasks'].each do |task|
         id = target['app']['id'].tr("/", "")
+
+        if File.exist?("#{synapse_config_path}/#{id}.json}") == false
+          synapse_conf = ichnaea.build_synapse_json(zk_hosts, app)
+          ichnaea.write_config(synapse_config_path, id, synapse_conf)
+        end
 
         if task['host'].include?(host) 
           nerve_conf = ichnaea.build_nerve_json(ip, zk_hosts, app, task)
@@ -39,7 +41,5 @@ apps['apps'].each do |app|
         end
       end
     end
-  else
-    puts "Not writing conf for #{app['id']} since there is no healthChecks configured in marathon"
   end
 end
