6bfe959f46e8cfa70bc93675dc6ab94758560751
Charles Martinot <charles@pocketplaylab.com>
2015-11-18 18:15:24 +0700

    simplified writing conditions

--- main.rb
+++ main.rb
@@ -11,7 +11,6 @@ nerve_config_path = ENV.fetch('NERVE') || '/etc/nerve/services'
 synapse_config_path = ENV.fetch('SYNAPSE') || '/etc/synapse/services'
 marathon = ENV.fetch('MARATHON')
 
-
 ichnaea = Ichnaea.new
 
 apps = ichnaea.request("http://#{marathon}:8080/v2/apps")
@@ -19,11 +18,10 @@ apps = ichnaea.request("http://#{marathon}:8080/v2/apps")
 apps['apps'].each do |app|
   target = ichnaea.request("http://#{marathon}:8080/v2/apps/#{app['id']}")
   if target['app']['healthChecks'].empty? == false
-    wrote_file = false
-    i = 1
-
     if target['app']['container']['docker'].key?('portMappings')
       target['app']['tasks'].each do |task|
+        # wrote_file = false
+        i = 1
         id = target['app']['id'].tr("/", "")
 
         if File.exist?("#{synapse_config_path}/#{id}.json}") == false
@@ -31,12 +29,13 @@ apps['apps'].each do |app|
           ichnaea.write_config(synapse_config_path, id, synapse_conf)
         end
 
-        if task['host'].include?(host) 
+        if task['host'].include?(host) && File.exist?("#{nerve_config_path}/#{id}#{i}.json") == false
           nerve_conf = ichnaea.build_nerve_json(ip, zk_hosts, app, task)
           ichnaea.write_config(nerve_config_path, "#{id}#{i}", nerve_conf)
-          wrote_file = true
+          # wrote_file = true
           i += 1
-        elsif wrote_file == false && File.exist?("#{nerve_config_path}/#{id}#{i}.json")
+        # elsif wrote_file == false && File.exist?("#{nerve_config_path}/#{id}#{i}.json")
+        elsif File.exist?("#{nerve_config_path}/#{id}#{i}.json")
           ichnaea.delete_config(nerve_config_path, id)
         end
       end
